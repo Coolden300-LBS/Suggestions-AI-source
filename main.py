c235bc9dbb7ea89325c3abc3d338f4e24cdd8b2a
Coolden300 <daniil.kosenko-suprun@elev.ga.lbs.se>
2026-05-06 11:07:04 +0200

    added stuff to upload

---
+++ main.py
@@ -0,0 +1,77 @@
+import sys
+import json
+from PySide6 import QtWidgets, QtGui
+from PySide6.QtUiTools import QUiLoader
+from pathlib import Path
+
+FOLDER_PATH = str(Path(__file__).resolve().parent)
+
+# gui tools setup
+app = QtWidgets.QApplication(sys.argv)
+loader = QUiLoader()
+window = loader.load(f"{FOLDER_PATH}/Suggestions.ui")
+
+# gui elements setup
+genre_input = window.findChild(QtWidgets.QLineEdit, "genre_preferences")
+mood_input = window.findChild(QtWidgets.QLineEdit, "mood_preferences")
+list_widget = window.findChild(QtWidgets.QListWidget, "film_list")
+
+# create film card for list manually with code 
+# (I couldn't find a way to make a visual template that can be ctrl+c ctld-v by script)
+
+class MovieCard(QtWidgets.QWidget):
+    def __init__(self, title: str, year: int, genres, moods):
+        super().__init__()
+
+        layout = QtWidgets.QHBoxLayout(self)
+
+        # film cover on the left
+        self.image = QtWidgets.QLabel()
+        self.image.setFixedSize(60, 90)
+
+        # add image cover (if exists)
+        image_path = f"{FOLDER_PATH}/FilmCovers/{title}"
+        if Path(image_path).exists:
+            pixmap = QtGui.QPixmap(image_path)
+            self.image.setPixmap(pixmap.scaled(60, 90))
+
+        # container widget for text layout
+        text_widget = QtWidgets.QWidget()
+        text_layout = QtWidgets.QVBoxLayout(text_widget)
+
+        # labels
+        self.title = QtWidgets.QLabel(f"{title} ({year})")
+        self.genres = QtWidgets.QLabel(f"Genres: {genres}")
+        self.moods = QtWidgets.QLabel(f"Moods: {moods}")
+
+        text_layout.addWidget(self.title)
+        text_layout.addWidget(self.genres)
+        text_layout.addWidget(self.moods)
+
+        # add widget (NOT layout)
+        layout.addWidget(self.image)
+        layout.addWidget(text_widget)
+
+# functions
+def add_movie(title: str, year: int, genres, moods):
+    item = QtWidgets.QListWidgetItem()
+    card = MovieCard(title, year, genres, moods)
+
+    item.setSizeHint(card.sizeHint())
+    list_widget.addItem(item)
+    list_widget.setItemWidget(item, card)
+
+def fill_list():
+    add_movie("Big Brother Is Watching", 1984, "realism", "sad")
+
+# input connections
+#genre_input.textEdited.connect()
+#mood_input.textEdited.connect()
+
+# application run
+fill_list()
+window.show()
+sys.exit(app.exec())
+
+
+# TODO: add more stuff to data and sorting algorithm
